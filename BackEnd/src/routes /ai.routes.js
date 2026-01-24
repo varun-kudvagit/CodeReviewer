8b90594b943a1d29cbb89f86919d92beb4f3337d
Varun Kudva <varunkudva111@gmail.com>
2026-01-24 15:15:46 +0530

    Update ai.routes.js

--- BackEnd/src/routes /ai.routes.js
+++ BackEnd/src/routes /ai.routes.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const aiController = require("../controllers/ai.controller")
-
-const router = express.Router();
-
-
+const express = require('express')
+const router = express.Router()
+const aiController = require('../controllers/ai.controller.js')  // Add .js
 router.post("/get-review", aiController.getReview)
+//router.post('/review', aiController.reviewCode)
 
-
-module.exports = router;    
+module.exports = router
